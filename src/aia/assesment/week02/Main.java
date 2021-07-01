b73662115a8cd61a40d72b9cdfe5a66cff344589
Darwin <id.darwins@gmail.com>
2021-07-01 10:37:50 +0700

    add assesment week 02

--- src/aia/assesment/week02/Main.java
+++ src/aia/assesment/week02/Main.java
@@ -9,17 +9,17 @@ public class Main {
 		Circle newCircle = new Circle();
 		newCircle.setRadius(20.0);
 		newCircle.toString();
-		System.out.println(newCircle.getPerimeter(150.0));
+		System.out.println("Circle Perimeter: " + newCircle.getPerimeter(150.0));
 
 		// declare for rectangle
 		Rectangle newRectangle = new Rectangle();
 		newRectangle.setWidth(30.0);
 		newCircle.toString();
-		System.out.println(newRectangle.getArea(100.0));
+		System.out.println("Rectangle Area: " + newRectangle.getArea(100.0));
 
 		// declare for square
 		Square newSquare = new Square();
 		newCircle.toString();
-		System.out.println(newSquare.getArea(20.0));
+		System.out.println("Square Area: " + newSquare.getArea(20.0));
 	}
 }
